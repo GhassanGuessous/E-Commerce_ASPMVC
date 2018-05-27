569dbf58413f6b0a01fca703848cec5482e34f71
fouzialaajili <fouzialaajili@gmail.com>
2018-05-27 13:13:26 +0200

    Les statistiiques

---
+++ E-Commerce_Solution/E-Commerce/Controllers/StatistiquesController.cs
@@ -0,0 +1,41 @@
+﻿using E_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class StatistiquesController : Controller
+    {
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response;
+        IEnumerable<Categorie> listcategorie;
+        IEnumerable<Article> listarticles;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public void Conction()
+        {
+            client.BaseAddress = new Uri("http://localhost:59467");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+        }
+        public JsonResult GetData()
+            {
+            Conction();
+           IEnumerable<NumberCommandePerArticle> Details = new List<NumberCommandePerArticle>();
+            response = client.GetAsync("api/Statistique").Result;
+            Details = response.Content.ReadAsAsync<IEnumerable<NumberCommandePerArticle>>().Result;
+
+         
+            return Json(Details, JsonRequestBehavior.AllowGet);
+            }
+
+
+        }
+    }
---
+++ E-Commerce_Solution/WebAPI_E-Commerce/Service/ICommande.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI_E_Commerce.Models;
+
+namespace WebAPI_E_Commerce.Service
+{
+    interface ICommande
+    {
+       IEnumerable<NumberCommandePerArticle> GetNumberCommandePerArticle();
+    }
+}
